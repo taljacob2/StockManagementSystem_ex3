bd19609651c91db1b5c0130cda4276c00403a3ea
Tal Jacob <taljacob2@gmail.com>
2021-07-09 15:56:27 +0300

    after deleting index.html

--- web/src/main/java/com/team/web/ui/controller/index/IndexController.java
+++ web/src/main/java/com/team/web/ui/controller/index/IndexController.java
@@ -5,13 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController @RequestMapping("index") public class IndexController {
+@RestController @RequestMapping(value = {"/", "index"})
+public class IndexController {
 
     @GetMapping public ModelAndView index() {
 
         // Show the 'index' page:
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("mainweb/index");
+        modelAndView.setViewName("mainweb/home");
         return modelAndView;
     }
 
