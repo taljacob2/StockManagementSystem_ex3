aaeaf16fd0425a1ea4b5f056b26974a832ccccb2
Tal Jacob <taljacob2@gmail.com>
2021-07-10 11:56:35 +0300

    backup bugged

--- web/src/main/resources/static/storage/storage-logic.js
+++ web/src/main/resources/static/storage/storage-logic.js
@@ -1,18 +1,26 @@
-window.addEventListener("pageshow", () => {
-    checkUserExistence();
-});
 
 
-function checkUserExistence() {
+// window.addEventListener("pageshow", () => {
+//     checkUserExistence();
+//
+// });
 
-    // check if this user is signed in.
-    if (localStorage.getItem("user") === null) {
-        console.log("user doesnt exist");
-        location.replace("home.html");
-    }
+//
+// function checkUserExistence() {
+//
+//     // check if this user is signed in.
+//     if (localStorage.getItem("user") === null) {
+//         console.log("user doesnt exist");
+//         location.replace("home.html");
+//     }
+//
+// }
 
-}
-
-function logout() {
-    localStorage.removeItem("user");
-}
+// function logout() {
+//     localStorage.removeItem("user");
+// }
+//
+// function getUser() {
+//     document.getElementById("span-username").innerText = localStorage.getItem("user");
+//     //return localStorage.getItem("user");
+// }
