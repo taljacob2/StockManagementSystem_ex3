9af015ba15eac887023f27add3b3e6ad1fad1e29
Tal Jacob <taljacob2@gmail.com>
2021-07-10 14:14:33 +0300

    error 404 fixed

--- web/src/main/java/com/team/web/ui/controller/error/ErrorControllerImpl.java
+++ web/src/main/java/com/team/web/ui/controller/error/ErrorControllerImpl.java
@@ -3,20 +3,32 @@ package com.team.web.ui.controller.error;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
-public class ErrorControllerImpl implements ErrorController {
+@Controller public class ErrorControllerImpl implements ErrorController {
 
     private static final String PATH = "/error";
 
-    @RequestMapping(value = PATH)
-    public String error() {
-        return "mainweb/error";
+    @RequestMapping(value = PATH) public ModelAndView error() {
+
+        // Redirect to 'error 404':
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/error404");
+        return modelAndView;
     }
 
-    @Override
-    public String getErrorPath() {
+    @GetMapping("error404") public ModelAndView error404() {
+
+        // Show the 'error 404' page:
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("mainweb/error");
+        return modelAndView;
+    }
+
+
+    @Override public String getErrorPath() {
         return PATH;
     }
 }
