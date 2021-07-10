4e4c88b55f2c252405de61d18711202760bea7e4
Tal Jacob <taljacob2@gmail.com>
2021-07-10 12:21:05 +0300

    works great

--- web/src/main/resources/static/storage/storage-logic.js
+++ web/src/main/resources/static/storage/storage-logic.js
@@ -1,19 +1,19 @@
 getUser();
 
-window.addEventListener("pageshow", () => {
-    checkUserExistence();
-});
-
-
-function checkUserExistence() {
-
-    // check if this user is signed in.
-    if (localStorage.getItem("user") === null) {
-        console.log("user doesnt exist");
-        location.replace("home.html");
-    }
-
-}
+// window.addEventListener("pageshow", () => {
+//     checkUserExistence();
+// });
+//
+//
+// function checkUserExistence() {
+//
+//     // check if this user is signed in.
+//     if (localStorage.getItem("user") === null) {
+//         console.log("user doesnt exist");
+//         location.replace("home.html");
+//     }
+//
+// }
 
 function logout() {
     localStorage.removeItem("user");
