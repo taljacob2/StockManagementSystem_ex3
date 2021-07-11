9bd547707c5054bab400d68a0e8c6d2ef224e0d0
Tal Jacob <taljacob2@gmail.com>
2021-07-11 22:35:23 +0300

    ready to fix heavy.xml and then light.xml

--- web/src/main/java/com/team/web/WebApplication.java
+++ web/src/main/java/com/team/web/WebApplication.java
@@ -1,10 +1,8 @@
 package com.team.web;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +10,6 @@ import timestamp.TimeStamp;
 
 @Slf4j
 @SpringBootApplication public class WebApplication{
-
-    @Autowired private Jaxb2Marshaller marshaller;
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
