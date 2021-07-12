286863d167c134ead0a37451c8d0bf53fe916f16
Tal Jacob <taljacob2@gmail.com>
2021-07-12 16:20:19 +0300

    ajax works perfectly with GENERIC pull-all6

--- web/src/main/java/com/team/web/ui/controller/hello/HelloController.java
+++ web/src/main/java/com/team/web/ui/controller/hello/HelloController.java
@@ -7,31 +7,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import timestamp.TimeStamp;
 
 // TODO: this is a test Controller.
+
+/**
+ * AJAX GUIDE: https://www.youtube.com/watch?v=Y_w9KjOrEXk&ab_channel=JavaTechie
+ */
 @Controller @RequestMapping("hello") public class HelloController {
 
     @RequestMapping("{name}") String hello(@PathVariable String name,
                                            Model model) {
         model.addAttribute("name", name);
         model.addAttribute("time", TimeStamp.getTimeStamp());
-        // return "Hello to you " + name + ", the time is: " +
-        //         TimeStamp.getTimeStamp();
         return "hello";
     }
-
-
-    //
-    // @GetMapping("getTest") public ResponseEntity<Object> getUser() {
-    //     ServiceResponse<String> response =
-    //             new ServiceResponse<>("success", TimeStamp.getTimeStamp());
-    //     return new ResponseEntity<Object>(response, HttpStatus.OK);
-    // }
-
-
-    // @RequestMapping(value = "getTime", method = RequestMethod.POST) @ResponseBody
-    // public String getTime(@RequestParam("timeStamp") String timeStamp) {
-    //
-    //
-    //     return TimeStamp.getTimeStamp();
-    // }
 
 }
