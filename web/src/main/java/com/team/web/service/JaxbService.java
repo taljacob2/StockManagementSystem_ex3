45defe995a898dd5494c986983308ed949fcf4f0
Tal Jacob <taljacob2@gmail.com>
2021-07-12 03:29:20 +0300

    jaxb unmarshal works perfectly!!!

--- web/src/main/java/com/team/web/service/JaxbService.java
+++ web/src/main/java/com/team/web/service/JaxbService.java
@@ -2,6 +2,12 @@ package com.team.web.service;
 
 import user.User;
 
+/**
+ * An <i>interface</i> that contains other methods for the {@code Service} of
+ * handling {@link user.User} transfers through <i>database</i>.
+ *
+ * @see com.team.web.service.impl.JaxbServiceImpl
+ */
 public interface JaxbService {
 
     void marshal();
