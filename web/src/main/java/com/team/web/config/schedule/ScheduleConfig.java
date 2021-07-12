dd6aad1ac2abfd1f08dd37c3427e3fb05a269563
Tal Jacob <taljacob2@gmail.com>
2021-07-12 10:58:45 +0300

    after creating scheduling config

---
+++ web/src/main/java/com/team/web/config/schedule/ScheduleConfig.java
@@ -0,0 +1,9 @@
+package com.team.web.config.schedule;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration @EnableScheduling
+@ConditionalOnProperty(name = "scheduling.enabled", matchIfMissing = true)
+public class ScheduleConfig {}
