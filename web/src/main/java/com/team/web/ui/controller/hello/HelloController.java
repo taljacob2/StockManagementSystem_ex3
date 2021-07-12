2b07273791b16432931fd409e71f3da34bd1e755
Tal Jacob <taljacob2@gmail.com>
2021-07-12 16:03:35 +0300

    ajax works perfectly with GENERIC pull-all

--- web/src/main/java/com/team/web/ui/controller/hello/HelloController.java
+++ web/src/main/java/com/team/web/ui/controller/hello/HelloController.java
@@ -1,11 +1,7 @@
 package com.team.web.ui.controller.hello;
 
-import com.team.web.ui.model.response.ServiceResponse;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import timestamp.TimeStamp;
@@ -22,24 +18,13 @@ import timestamp.TimeStamp;
         return "hello";
     }
 
-    /**
-     * GUIDE : https://www.youtube.com/watch?v=HCgLRKeQnGY
-     *
-     * @return response of {@code timeStamp}.
-     */
-    // @RequestMapping(value = "getTime", method = RequestMethod.GET)
-    // public @ResponseBody String getTime() throws JSONException {
-    //     System.out.println("responding..."); // DE-BUG - checking here
+
     //
-    //     JSONObject resObj = new JSONObject();
-    //     resObj.put("time", TimeStamp.getTimeStamp());
-    //     return resObj.toString();
+    // @GetMapping("getTest") public ResponseEntity<Object> getUser() {
+    //     ServiceResponse<String> response =
+    //             new ServiceResponse<>("success", TimeStamp.getTimeStamp());
+    //     return new ResponseEntity<Object>(response, HttpStatus.OK);
     // }
-    @GetMapping("getTest") public ResponseEntity<Object> getUser() {
-        ServiceResponse<String> response =
-                new ServiceResponse<>("success", TimeStamp.getTimeStamp());
-        return new ResponseEntity<Object>(response, HttpStatus.OK);
-    }
 
 
     // @RequestMapping(value = "getTime", method = RequestMethod.POST) @ResponseBody
--- web/src/main/java/com/team/web/ui/controller/hello/TimeController.java
+++ web/src/main/java/com/team/web/ui/controller/hello/TimeController.java
@@ -1,18 +1,13 @@
 package com.team.web.ui.controller.hello;
 
-import com.team.web.ui.model.response.ServiceResponse;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import timestamp.TimeStamp;
 
 @RestController public class TimeController {
 
-    @GetMapping("getTest") public ResponseEntity<Object> getUser() {
-        ServiceResponse<String> response =
-                new ServiceResponse<>("success", TimeStamp.getTimeStamp());
-        return new ResponseEntity<Object>(response, HttpStatus.OK);
-    }
+    // @GetMapping("getTest") public ResponseEntity<Object> getUser() {
+    //     ServiceResponse<String> response =
+    //             new ServiceResponse<>("success", TimeStamp.getTimeStamp());
+    //     return new ResponseEntity<Object>(response, HttpStatus.OK);
+    // }
 
 }
