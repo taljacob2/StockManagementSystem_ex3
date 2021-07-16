4e17f55ce60fd87ebfce7cb2d2785655bf6ddf77
Tal Jacob <taljacob2@gmail.com>
2021-07-16 19:13:21 +0300

    ajax-generic-get.js ajaxGetHello works dataType "text" gets the "time"

--- web/src/main/java/com/team/web/ui/controller/hello/HelloController.java
+++ web/src/main/java/com/team/web/ui/controller/hello/HelloController.java
@@ -2,8 +2,9 @@ package com.team.web.ui.controller.hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import timestamp.TimeStamp;
 
 // TODO: this is a test Controller.
@@ -13,11 +14,23 @@ import timestamp.TimeStamp;
  */
 @Controller @RequestMapping("hello") public class HelloController {
 
-    @RequestMapping("{name}") String hello(@PathVariable String name,
-                                           Model model) {
-        model.addAttribute("name", name);
+    // @RequestMapping("{name}") String hello(@PathVariable String name,
+    //                                        Model model) {
+    //     model.addAttribute("name", name);
+    //     model.addAttribute("time", TimeStamp.getTimeStamp());
+    //     return "hello";
+    // }
+
+    // TODO: checking
+    @GetMapping("ye") String ye(Model model) {
+        System.out.println("reached ye");
         model.addAttribute("time", TimeStamp.getTimeStamp());
         return "hello";
+    }
+
+    @GetMapping("time") @ResponseBody String time(Model model) {
+        System.out.println("reached time");
+        return TimeStamp.getTimeStamp();
     }
 
 }
--- web/src/main/resources/static/ajax/ajax-generic-get.js
+++ web/src/main/resources/static/ajax/ajax-generic-get.js
@@ -1,5 +1,21 @@
-// MUST set the id tag of the 'html' to be: <html id="thisHTML"></html>
+/**
+ * Extracts the pure name of the "web" application module.
+ * @returns {string}
+ */
+function webModuleName() {
 
+    // Get the first letter in the "web" application module name.
+    var pathWithoutLeadingSlash = window.location.pathname.substring(1);
+
+    // Extract the last index of the "web" application module.
+    var contextPathEndIndex = pathWithoutLeadingSlash.indexOf('/');
+
+    // Substring the path to be the "web" application module only:
+    return pathWithoutLeadingSlash.substring(0, contextPathEndIndex)
+}
+
+
+// MUST set the id tag of the 'html' to be: <html id="thisHTML"></html>
 let htmlElement = document.getElementById("thisHTML");
 
 function ajaxGenericGet() {
@@ -18,4 +34,27 @@ function ajaxGenericGet() {
     });
 }
 
-setInterval(ajaxGenericGet, 1000);
+function ajaxGetHello() {
+
+    // DO GET
+    $.ajax({
+        type: "GET",
+        url: "/" + webModuleName() + "/hello/time",
+        // url: "/web/hello/123123", /* TODO: WORKS! */
+        dataType: "text",
+
+        success: function (result) {
+            console.log(result)
+            htmlElement.innerHTML = result;
+        },
+        error: function (e) {
+            htmlElement.value = "ERROR: FAIL";
+        }
+    });
+}
+
+ajaxGetHello();
+
+// setInterval(ajaxGetHello, 1000);
+
+// setInterval(ajaxGenericGet, 1000);
