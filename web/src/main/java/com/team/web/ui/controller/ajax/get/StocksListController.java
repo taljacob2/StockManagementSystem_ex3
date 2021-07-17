f5aae04a4ace9490ac8cfd97d2a407a441a98b84
Tal Jacob <taljacob2@gmail.com>
2021-07-17 15:47:56 +0300

    after creating ajax package

--- web/src/main/java/com/team/web/ui/controller/ajax/get/StocksListController.java
+++ web/src/main/java/com/team/web/ui/controller/ajax/get/StocksListController.java
@@ -1,4 +1,4 @@
-package com.team.web.ui.controller.signed;
+package com.team.web.ui.controller.ajax.get;
 
 import engine.Engine;
 import org.springframework.stereotype.Controller;
@@ -7,18 +7,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import stock.Stocks;
 
-@Controller @RequestMapping("signed") public class SignedController {
+@Controller @RequestMapping("stocksList") public class StocksListController {
 
     /**
-     * Show the <tt>/signed</tt> page.
+     * Responding {@code fragment} of <i>stocksTableList</i> to present all
+     * stocks in the system.
      *
-     * @param model
-     * @return
+     * @return {@code fragment}.
      */
-    @GetMapping public String signed(Model model) {
+    @GetMapping public String getStocksList(Model model) {
         Stocks stocks = Engine.getStocksForced();
         model.addAttribute("stocksList", stocks.getCollection());
-        return "mainweb/signed";
+        return "fragments/stocks-table-fragment :: stocksTableFragment";
     }
 
 }
