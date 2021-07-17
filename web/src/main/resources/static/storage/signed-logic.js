7778134f59e66392154963118a02bd71bcedb30a
Tal Jacob <taljacob2@gmail.com>
2021-07-17 10:24:19 +0300

    change name to of storage-logic.js to signed-logic.js

--- web/src/main/resources/static/storage/signed-logic.js
+++ web/src/main/resources/static/storage/signed-logic.js
@@ -1,19 +1,4 @@
 
-// window.addEventListener("pageshow", () => {
-//     checkUserExistence();
-// });
-//
-//
-// function checkUserExistence() {
-//
-//     // check if this user is signed in.
-//     if (localStorage.getItem("user") === null) {
-//         console.log("user doesnt exist");
-//         location.replace("home");
-//     }
-//
-// }
-
 function logout() {
     localStorage.removeItem("user");
 }
