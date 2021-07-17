247177dd02bfd0164b07da2f11151abf838bddec
Tal Jacob <taljacob2@gmail.com>
2021-07-17 17:01:02 +0300

    after making user json on AjaxUserController.java 2

--- web/src/main/java/com/team/web/ui/controller/ajax/AjaxUserController.java
+++ web/src/main/java/com/team/web/ui/controller/ajax/AjaxUserController.java
@@ -1,14 +1,16 @@
 package com.team.web.ui.controller.ajax;
 
 import engine.Engine;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import user.User;
 
-@Controller @RequestMapping("user") public class AjaxUserController {
+@Slf4j @Controller @RequestMapping("user") public class AjaxUserController {
 
     /**
      * <i>Finds by name</i> the {@link User} that is <i>name</i> is
@@ -20,11 +22,14 @@ import user.User;
      * @return produces {@code "application/json"} of {@link User}.
      */
     @GetMapping(value = "{userName}", produces = "application/json")
-    public User getUser(@PathVariable String userName, Model model) {
+    @ResponseBody public User getUser(@PathVariable("userName") String userName,
+                                      Model model) {
 
         User user = Engine.findUserByNameForced(userName);
         model.addAttribute("user", user);
 
+
+        log.info("user {}", user); // DEBUG
         return user;
     }
 
