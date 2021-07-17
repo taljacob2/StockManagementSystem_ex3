b122cd748f700d26d696705ba39bfc8975b8b95a
Tal Jacob <taljacob2@gmail.com>
2021-07-17 16:12:55 +0300

    after making user json on AjaxUserController.java

--- engine/src/engine/Engine.java
+++ engine/src/engine/Engine.java
@@ -873,18 +873,6 @@ public class Engine {
      * field. In case the {@link #users} field is {@code null}, then return
      * null.
      *
-     * @param user the {@link User} to be searched in the {@link Users} field.
-     */
-    public static User findUserByNameForced(User user) {
-        String userName = user.getName();
-        return findUserByNameForced(userName);
-    }
-
-    /**
-     * Finds a {@link User} by {@link User#getName()} in the {@link #users}
-     * field. In case the {@link #users} field is {@code null}, then return
-     * null.
-     *
      * @param name the {@link User#getName()} to be searched in the {@link
      *             Users} field.
      */
--- web/src/main/java/com/team/web/ui/controller/ajax/AjaxUserController.java
+++ web/src/main/java/com/team/web/ui/controller/ajax/AjaxUserController.java
@@ -1,3 +1,33 @@
 package com.team.web.ui.controller.ajax;
 
-public class AjaxUserController {}
+import engine.Engine;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import user.User;
+
+@Controller @RequestMapping("user") public class AjaxUserController {
+
+    /**
+     * <i>Finds by name</i> the {@link User} that is <i>name</i> is
+     * provided. Returns the {@link User} as a {@code "application /json"}
+     * response.
+     *
+     * @param userName the userName to find the {@link User} by.
+     * @param model    the next HTML model.
+     * @return produces {@code "application/json"} of {@link User}.
+     */
+    @GetMapping(value = "{userName}", produces = "application/json")
+    public User getUser(@PathVariable String userName, Model model) {
+
+        User user = Engine.findUserByNameForced(userName);
+        model.addAttribute("user", user);
+
+        return user;
+    }
+
+}
+
+
