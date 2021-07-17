f9bbf5b79175ed0b5ef8fa45944fa063afbf4315
Tal Jacob <taljacob2@gmail.com>
2021-07-17 11:22:19 +0300

    after restoring ErrorControllerImpl

--- web/src/main/java/com/team/web/ui/controller/error/ErrorControllerImpl.java
+++ web/src/main/java/com/team/web/ui/controller/error/ErrorControllerImpl.java
@@ -15,8 +15,8 @@ import org.springframework.web.servlet.ModelAndView;
 
         // Redirect to 'error 404':
         ModelAndView modelAndView = new ModelAndView();
-        // modelAndView.setViewName("redirect:/error404"); // TODO: return this
-        modelAndView.setViewName("mainweb/error");
+        modelAndView.setViewName("redirect:/error404");
+        // modelAndView.setViewName("mainweb/error");
         return modelAndView;
     }
 
