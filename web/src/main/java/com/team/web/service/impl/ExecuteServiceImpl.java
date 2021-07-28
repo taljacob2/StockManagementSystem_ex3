fa08c252ffaaaf1efc3f2ac1cdb551d9d5700ca2
Tal Jacob <taljacob2@gmail.com>
2021-07-28 16:18:45 +0300

    ExecuteServiceImpl works okay. but still : Note: The order has not been fulfilled in its entirety nor partially yet.

--- web/src/main/java/com/team/web/service/impl/ExecuteServiceImpl.java
+++ web/src/main/java/com/team/web/service/impl/ExecuteServiceImpl.java
@@ -1,5 +1,6 @@
 package com.team.web.service.impl;
 
+import com.team.shared.engine.data.execute.AfterExecutionOrderAndTransactionDTO;
 import com.team.shared.engine.data.order.Order;
 import com.team.shared.engine.data.stock.Stock;
 import com.team.shared.engine.engine.Engine;
@@ -9,14 +10,24 @@ import org.springframework.stereotype.Service;
 /**
  * A {@code Service} which serves for handling {@link Order} <i>executions</i>
  * performances to the <i>database</i> (Which on this project, stored in {@link
- * Engine#getAfterExecuteOrderAndTransactionDTO()}).
+ * Engine#getAfterExecutionOrderAndTransactionDTO()}).
  * <p>
  * This service is ready for future use of <i>database</i> implementations.
  * </p>
  */
 @Service public class ExecuteServiceImpl implements ExecuteService {
 
+    /**
+     * Warning: this implementation <b>does not</b> check that {@link
+     * Engine#getStocks()} {@code != null}.
+     *
+     * @param stock the {@link Stock} to execute the {@link Order} to.
+     * @param order the {@link Order} to pend execution.
+     */
     @Override public void executeOrder(Stock stock, Order order) {
 
+        // Calc this newly placed order with the matching already placed Orders:
+        Engine.calcOrdersOfASingleStock(
+                new AfterExecutionOrderAndTransactionDTO(), stock, order);
     }
 }
--- web/src/main/java/com/team/web/ui/controller/signed/order/OrderController.java
+++ web/src/main/java/com/team/web/ui/controller/signed/order/OrderController.java
@@ -4,14 +4,18 @@ import com.team.shared.engine.data.order.Order;
 import com.team.shared.engine.data.stock.Stock;
 import com.team.shared.engine.engine.Engine;
 import com.team.shared.engine.timestamp.TimeStamp;
+import com.team.web.service.ExecuteService;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 
 @Slf4j @RestController @RequestMapping("order") public class OrderController {
+
+    @Autowired ExecuteService executeService;
 
     @SneakyThrows @GetMapping("{stockSymbol}")
     public ModelAndView linkToStock(@PathVariable String stockSymbol,
@@ -43,8 +47,8 @@ import org.springframework.web.servlet.ModelAndView;
         log.info("order {}", order); // DEBUG
 
         // Make a transaction order:
-        // MenuUI.command_EXECUTE_TRANSACTION_ORDER(
-        //         Engine.getStockBySymbol(stockSymbol), order);
+        executeService
+                .executeOrder(Engine.getStockBySymbol(stockSymbol), order);
 
         ModelAndView modelAndView = new ModelAndView("redirect:/signed/user");
         if (Engine.findUserByNameForced(username).getRole().toString()
