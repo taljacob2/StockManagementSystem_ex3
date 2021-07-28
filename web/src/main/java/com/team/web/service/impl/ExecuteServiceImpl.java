5b9840de63c5788193374cad23ecc3b4b3b55220
Tal Jacob <taljacob2@gmail.com>
2021-07-28 16:50:47 +0300

    after Order insert

--- web/src/main/java/com/team/web/service/impl/ExecuteServiceImpl.java
+++ web/src/main/java/com/team/web/service/impl/ExecuteServiceImpl.java
@@ -2,6 +2,7 @@ package com.team.web.service.impl;
 
 import com.team.shared.engine.data.execute.AfterExecutionOrderAndTransactionDTO;
 import com.team.shared.engine.data.order.Order;
+import com.team.shared.engine.data.order.OrderDirection;
 import com.team.shared.engine.data.stock.Stock;
 import com.team.shared.engine.engine.Engine;
 import com.team.web.service.ExecuteService;
@@ -25,9 +26,27 @@ import org.springframework.stereotype.Service;
      * @param order the {@link Order} to pend execution.
      */
     @Override public void executeOrder(Stock stock, Order order) {
+        insertOrder(stock, order);
 
         // Calc this newly placed order with the matching already placed Orders:
         Engine.calcOrdersOfASingleStock(
                 new AfterExecutionOrderAndTransactionDTO(), stock, order);
+    }
+
+    /**
+     * Add the {@link Order} to the according 'awaiting orders' {@link
+     * java.util.Collection} in the {@link Stock#getDataBase()}.
+     *
+     * @param stock the {@link Stock} to execute the {@link Order} to.
+     * @param order the {@link Order} to pend execution.
+     */
+    private void insertOrder(Stock stock, Order order) {
+        if (order.getOrderDirection() == OrderDirection.BUY) {
+            stock.getDataBase().getAwaitingBuyOrders().getCollection()
+                    .sortedAdd(order);
+        } else if (order.getOrderDirection() == OrderDirection.SELL) {
+            stock.getDataBase().getAwaitingSellOrders().getCollection()
+                    .sortedAdd(order);
+        }
     }
 }
