b7f09c21ffd9dcfaa91da3c1c6178071867655a1
Tal Jacob <taljacob2@gmail.com>
2021-07-29 15:11:56 +0300

    Transaction success works perfectly !

--- web/src/main/java/com/team/shared/model/notification/Notification.java
+++ web/src/main/java/com/team/shared/model/notification/Notification.java
@@ -1,17 +1,21 @@
 package com.team.shared.model.notification;
 
 import com.team.shared.model.notification.type.NotificationType;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
 
-@Data @AllArgsConstructor public class Notification implements Serializable {
+@Data public class Notification implements Serializable {
 
     private static final long serialVersionUID = 7922611968289409036L;
 
-    private NotificationType type;
+    private String type;
     private String title;
     private String content;
 
+    public Notification(NotificationType type, String title, String content) {
+        this.type = type.toString().toLowerCase(); // MUST be LowerCase
+        this.title = title;
+        this.content = content;
+    }
 }
