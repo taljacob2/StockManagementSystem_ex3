e5a7073d4886f7d2059633f490efc34281219ea2
Tal Jacob <taljacob2@gmail.com>
2021-07-31 03:22:14 +0300

    wallet table works great with intervals. need to fix negative number representation.

--- web/src/main/java/com/team/web/ui/controller/ajax/fragments/user/wallet/AjaxFragmentsUserWalletController.java
+++ web/src/main/java/com/team/web/ui/controller/ajax/fragments/user/wallet/AjaxFragmentsUserWalletController.java
@@ -1,16 +1,22 @@
 package com.team.web.ui.controller.ajax.fragments.user.wallet;
 
+import com.team.shared.engine.data.user.User;
+import com.team.shared.engine.engine.Engine;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Responding a {@code fragment} to present in the system.
  */
-@Controller @RequestMapping("ajax/fragments/wallet")
+@Controller @RequestMapping("ajax/fragments/{userName}/wallet")
 public class AjaxFragmentsUserWalletController {
 
-    @GetMapping public String getWalletFragment() {
+    @GetMapping public String getWalletFragment(@PathVariable("userName") String userName, Model model) {
+        User user = Engine.findUserByNameForced(userName);
+        model.addAttribute("user", user);
         return "fragments/user/wallet/user-wallet-fragment :: walletFragment";
     }
 
