b3fa17da1e28dbd913c0d9aedcfe6032a5eff456
Tal Jacob <taljacob2@gmail.com>
2021-08-01 07:14:29 +0300

    after this-stock table

--- web/src/main/java/com/team/web/ui/controller/ajax/fragments/stock/AjaxThisStockController.java
+++ web/src/main/java/com/team/web/ui/controller/ajax/fragments/stock/AjaxThisStockController.java
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j @Controller
-@RequestMapping("ajax/fragments/stock/{stockSymbol}/thisStock")
-public class AjaxStockTransactionsController {
+@RequestMapping("ajax/fragments/stock/{stockSymbol}/transactions")
+public class AjaxThisStockController {
 
     @SneakyThrows @GetMapping public String getStockTransactions(
             @PathVariable("stockSymbol") String stockSymbol, Model model) {
         model.addAttribute("stock", Engine.getStockBySymbol(stockSymbol));
-        return "fragments/users/stock/this-stock-table-fragment :: thisStockTableFragment";
+        return "fragments/users/stock/stock-transactions-table-fragment :: stockTransactionsTableFragment";
     }
 
 }
