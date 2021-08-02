6cbd8f080c1e00c0c8fe556e3d2ed5a86d2cb3ff
taljacob2 <taljacob2@gmail.com>
2021-08-02 16:54:40 +0300

    for test

--- web/src/main/java/com/team/shared/dto/UserDTO.java
+++ web/src/main/java/com/team/shared/dto/UserDTO.java
@@ -1,6 +1,8 @@
 package com.team.shared.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
@@ -8,7 +10,7 @@ import java.io.Serializable;
  * A {@code class} which is used for data transfers between different layers in
  * the <tt>Spring</tt> framework.
  */
-@Data public class UserDTO implements Serializable {
+@AllArgsConstructor @NoArgsConstructor @Data public class UserDTO implements Serializable {
     private static final long serialVersionUID = 5143034332788520792L;
 
     private String name;
--- web/src/main/java/com/team/web/WebApplication.java
+++ web/src/main/java/com/team/web/WebApplication.java
@@ -1,14 +1,46 @@
 package com.team.web;
 
-import lombok.extern.slf4j.Slf4j;
+import com.team.shared.dto.UserDTO;
+import com.team.shared.engine.data.order.Order;
+import com.team.shared.engine.data.order.OrderDirection;
+import com.team.shared.engine.data.order.OrderType;
+import com.team.shared.engine.data.stock.Stock;
+import com.team.shared.engine.engine.Engine;
+import com.team.web.service.ExecuteService;
+import com.team.web.service.JaxbService;
+import com.team.web.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
-@SpringBootApplication public class WebApplication{
+@SpringBootApplication public class WebApplication
+        implements CommandLineRunner {
+
+    @Autowired UserService userService;
+
+    @Autowired JaxbService jaxbService;
+
+    @Autowired ExecuteService executeService;
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
 
+    @Override public void run(String... args) throws Exception {
+
+        // DEBUG : for tests:
+        userService.createUser(new UserDTO("tal", "USER"));
+        jaxbService.unmarshal(Engine.findUserByNameForced("tal"),
+                "C:\\Tal\\Code\\java\\rolling_ex_3\\XMLresources\\heaver-user.xml");
+        Stock stock = Engine.getStockBySymbol("GOOGL");
+        executeService.executeOrder(stock,new Order(OrderDirection.BUY,
+                OrderType.LMT,5,110,"tal"));
+        executeService.executeOrder(stock,new Order(OrderDirection.BUY,
+                OrderType.LMT,10,100,"tal"));
+        executeService.executeOrder(stock,new Order(OrderDirection.SELL,
+                OrderType.LMT,10,100,"tal"));
+        executeService.executeOrder(stock,new Order(OrderDirection.SELL,
+                OrderType.LMT,10,110,"tal"));
+    }
 }
