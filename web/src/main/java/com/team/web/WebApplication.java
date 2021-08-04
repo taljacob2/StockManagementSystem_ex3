db421cea85b041dc2cdf8cc36025c311168b602e
Tal Jacob <taljacob2@gmail.com>
2021-08-04 13:56:34 +0300

    done 2

--- web/src/main/java/com/team/web/WebApplication.java
+++ web/src/main/java/com/team/web/WebApplication.java
@@ -4,7 +4,7 @@ import com.team.retention.IgnoreDuringScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@IgnoreDuringScan @SpringBootApplication public class WebApplication{
+@IgnoreDuringScan @SpringBootApplication public class WebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
